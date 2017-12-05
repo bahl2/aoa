4b27bf93483ed91146cc44ecbf09115ed7526370
MatheusAlmeidaAssis <m.almeidadeassis@gmail.com>
2017-12-05 00:35:49 -0200

    documengtação

---
+++ Assets/Scripts/Objetos/Ranking.cs
@@ -0,0 +1,56 @@
+﻿using Assets.Scripts.Global;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Objetos
+{
+    [Serializable]
+    public class Ranking
+    {
+        public string Id, nome, tempo, dementadores, esqueletos;
+    }
+
+    public class RankingList
+    {
+        public List<Ranking> _Ranking;
+        public IEnumerator ListRanking()
+        {
+            WWW www = new WWW(GameTags.ServerHost() + "ranking/listRankingOrdenado");
+            yield return www;
+            if (www.error == null)
+            {
+                RankingData lRankingData = JsonUtility.FromJson<RankingData>(www.text);
+                _Ranking = new List<Ranking>();
+                foreach (Ranking lRanking in lRankingData.data)
+                {
+                    _Ranking.Add(lRanking);
+                    Debug.Log(lRanking.Id + " - " + lRanking.nome + " - Tempo: " + lRanking.tempo + " - Dementadores: " +
+                        lRanking.dementadores + " - Esqueletos: " + lRanking.esqueletos);
+                }
+            }
+        }
+
+        public IEnumerator SaveRanking(Ranking pRanking)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("nome", pRanking.nome);
+            form.AddField("tempo", pRanking.tempo);
+            form.AddField("dementadores", pRanking.dementadores);
+            form.AddField("esqueletos", pRanking.esqueletos);
+            WWW www = new WWW(GameTags.ServerHost() + "ranking/add", form);
+            yield return www;
+            if (www.error == null)
+                Debug.Log("ranking inserido!");
+            else
+                Debug.Log("Erro: " + www.error);
+        }
+
+    }
+
+    public class RankingData
+    {
+        public List<Ranking> data;
+    }
+}
---
+++ Assets/Scripts/UI/CarregaRanking.cs
@@ -0,0 +1,111 @@
+﻿using ArcadePUCCampinas;
+using Assets.Scripts.Global;
+using Assets.Scripts.Inimigos;
+using Assets.Scripts.Objetos;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class CarregaRanking : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _Caveiras;
+        [SerializeField]
+        private Text _Dementadores;
+        [SerializeField]
+        private Text _Tempo;
+        [SerializeField]
+        private Text _Nomes;
+        [SerializeField]
+        private InputField _SalvaNome;
+        [SerializeField]
+        private GameObject _Save;
+        [SerializeField]
+        private GameObject _Ranking;
+        private int _QtdEsqueletos;
+        private int _QtdDementadores;
+
+        private void OnEnable()
+        {
+            if (ControleFase._FimJogo)
+            {
+                _Ranking.SetActive(false);
+                _Save.SetActive(true);
+                Time.timeScale = 0;
+                _QtdEsqueletos = ControleFase._Caveiras - FindObjectsOfType<ControleCaveira>().Length;
+                _QtdDementadores = ControleFase._Dementadores - FindObjectsOfType<ControleDementador>().Length;
+                Cursor.visible = CFG._Plataforma == CFG.EPlataforma.PC;
+            }
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+        }
+
+        private void Update()
+        {
+            if (CFG._Plataforma == CFG.EPlataforma.Arcade)
+            {
+                if (_Ranking.activeSelf)
+                {
+                    if (InputArcade.Apertou(0, EControle.VERDE))
+                        Sair();
+                }
+                else
+                {
+                    if (InputArcade.Apertou(0, EControle.VERDE))
+                    {
+                        Salvar();
+                    }
+                }
+            }
+        }
+
+        private IEnumerator ListaRanking()
+        {
+            RankingList lRankingList = new RankingList();
+            yield return StartCoroutine(lRankingList.ListRanking());
+            _Save.SetActive(false);
+            _Ranking.SetActive(true);
+            _Nomes.text = string.Empty;
+            _Caveiras.text = string.Empty;
+            _Dementadores.text = string.Empty;
+            _Tempo.text = string.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                Ranking lRanking = lRankingList._Ranking[i];
+                _Nomes.text += string.Format("{0} - {1}\n", i + 1, lRanking.nome.Substring(0, 5));
+                _Caveiras.text += lRanking.esqueletos + "\n";
+                _Dementadores.text += lRanking.dementadores + "\n";
+                _Tempo.text += lRanking.tempo + "\n";
+            }
+        }
+
+        private IEnumerator SavarRanking()
+        {
+            RankingList lRankingList = new RankingList();
+            Ranking lRanking = new Ranking
+            {
+                nome = _SalvaNome.text,
+                tempo = FindObjectOfType<TimeTrial>()._Contador.text,
+                dementadores = _QtdDementadores.ToString(),
+                esqueletos = _QtdEsqueletos.ToString()
+            };
+            yield return StartCoroutine(lRankingList.SaveRanking(lRanking));
+            StartCoroutine(ListaRanking());
+        }
+
+        public void Sair()
+        {
+            Carrega.Cena = GameTags._Cenas[(int)GameTags.ECenas.MenuPrincipal];
+        }
+
+        public void Salvar()
+        {
+            StartCoroutine(SavarRanking());
+        }
+    }
+}
